01026774e98fe9539b4cf522dad9601678ef4f61
Saurabh-001-S <125719000+Saurabh-001-S@users.noreply.github.com>
2023-10-16 11:11:05 +0530

    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthPage, SecurePage } from "./Compenent/index";
 import './App.css';
 
 function App() {
+
+  const [userLogin, setUserLogin] = useState(false)
+  const [loginUserName, setLoginUserName] = useState("")
+
+  const handleState = (state, userName) => {
+    setUserLogin(state);
+    setLoginUserName(userName)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={
+            <SecurePage handleState={handleState} userLogin={userLogin} loginUserName={loginUserName} />
+          } />
+          <Route exact path='/authpage' element={<AuthPage handleState={handleState} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
